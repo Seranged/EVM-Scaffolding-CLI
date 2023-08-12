efa5b9052e33ecfe8f5a6fe515140d01eb741eae
Seranged <seranged@gmail.com>
2023-08-12 11:29:57 -0400

    v1.1.01 - fix git clone import (no more branch)

--- src/scripts/cloneRepo.ts
+++ src/scripts/cloneRepo.ts
@@ -2,7 +2,7 @@ import { exec } from 'child_process'
 
 export function cloneRepo(repoUrl: string, directory: string): Promise<void> {
   return new Promise((resolve, reject) => {
-    exec(`git clone -b ${repoUrl} ${directory}`, (error) => {
+    exec(`git clone ${repoUrl} ${directory}`, (error) => {
       if (error) {
         reject(error)
       } else {
